2c46198537d78d50e792feb98c336172b10eb10b
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-02-23 07:09:56 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="px-4 md:px-16 py-20">
+
+ 
+  <ULandingHero
+    title="Turn Your Vision into Reality"
+    description="Watch your dreams materialize before your eyes with us."
+  />
+  <div class="grid grid-cols-3 gap-6">
+    <ULandingCard
+    title="Color Palette"
+    description="Choose a primary and a gray color from your Tailwind CSS color palette. Components will be styled accordingly."
+    icon="i-heroicons-swatch"
+    color="emerald"
+  />
+    <ULandingCard
+    title="Color Palette"
+    description="Choose a primary and a gray color from your Tailwind CSS color palette. Components will be styled accordingly."
+    icon="i-heroicons-swatch"
+    color="emerald"
+  />
+    <ULandingCard
+    title="Color Palette"
+    description="Choose a primary and a gray color from your Tailwind CSS color palette. Components will be styled accordingly."
+    icon="i-heroicons-swatch"
+    color="emerald"
+  />
+    <ULandingCard
+    title="Color Palette"
+    description="Choose a primary and a gray color from your Tailwind CSS color palette. Components will be styled accordingly."
+    icon="i-heroicons-swatch"
+    color="emerald"
+  />
+  </div> </div>
+</template>
+
+
+<style scoped></style>
